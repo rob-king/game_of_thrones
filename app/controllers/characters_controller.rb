ed309cfbc7194a794498642c0cb198547da5e2c6
robert <robert.k25@gmail.com>
2016-11-08 11:21:57 -0500

    adds character editing functionality to view, fixes small controllre bug

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,8 +10,39 @@ class CharactersController < ApplicationController
   end
 
   def new
+    @house = House.find(params[:house_id])
+    @character = @house.characters.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create(character_params)
+    redirect_to house_character_path(@house, @character)
   end
 
   def edit
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to house_character_path(@house, @character)
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(
+    :name,
+    :gender,
+    :culture,
+    :born,
+    :died,
+    :title,
+    :alias
+    )
   end
 end
