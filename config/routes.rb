c41a6431d3a0d5e3810ff75777fb56344ad5d535
robert <robert.k25@gmail.com>
2016-11-07 23:44:39 -0500

    adds restful routes for both models

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'characters/index'
-
-  get 'characters/show'
-
-  get 'characters/new'
-
-  get 'characters/edit'
-
-  get 'houses/index'
-
-  get 'houses/show'
-
-  get 'houses/new'
-
-  get 'houses/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :houses do
+    resources :characters
+  end
 end
