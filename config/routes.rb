f92475aa6ba94b12ecafab5cb443807bb953cda8
robert <robert.k25@gmail.com>
2016-11-07 23:42:34 -0500

    adds generated files for controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'characters/index'
+
+  get 'characters/show'
+
+  get 'characters/new'
+
+  get 'characters/edit'
+
+  get 'houses/index'
+
+  get 'houses/show'
+
+  get 'houses/new'
+
+  get 'houses/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
